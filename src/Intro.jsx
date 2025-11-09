8c885cac6f3fcd42035a2df4701f53dbf3d293a4
Abhishek <abhishek.kabi.21@gmail.com>
2025-11-10 02:36:18 +0530

    Added new features

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -3,10 +3,10 @@ function Intro() {
     <>
       <div className="intro">
         <h2>What We Do</h2>
-        <div>
+        <div className="intro-p-div">
           <p>
             At Aphelion Labs Inc. we create cutting-edge web experiences that
-            engage and inspire users, combining sleek design with rub- st
+            engage and inspire users, combining sleek design with robust
             functionality.
           </p>
         </div>
