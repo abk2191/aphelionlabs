3f2c9507b5f515679e7e83b48539a02e19da6062
Abhishek <abhishek.kabi.21@gmail.com>
2025-11-10 02:56:03 +0530

    Added new features

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -3,8 +3,10 @@ function Banner() {
     <>
       <div className="banner">
         <div className="banner-heading">
-          <h2>Digital Solutions</h2>
-          <h2>for the Modern Web</h2>
+          <div>
+            <h2>Digital Solutions</h2>
+            <h2>for the Modern Web</h2>
+          </div>
           <p>
             We design and develop innovative web applications with a focus on
             performance and user experience.
