6dd16a5443937369d6356a6c905f07b0748ee426
Abhishek <abhishek.kabi.21@gmail.com>
2025-11-10 13:31:13 +0530

    Added new features

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,22 @@ import Navbar from "./Navbar";
 import Banner from "./Banner";
 import Intro from "./Intro";
 import Description from "./Description";
+import Sidebar from "./Sidebar";
 
 function App() {
+  const [sideBarVisibility, setSideBarVisibility] = useState(false);
+  const [isHamMenuClicked, setIsHamMenuClicked] = useState(false);
+  function handleSideBar() {
+    setSideBarVisibility((prev) => !prev);
+    setIsHamMenuClicked((prev) => !prev);
+  }
   return (
     <>
-      <Navbar />
+      <Navbar
+        handleSideBar={handleSideBar}
+        isHamMenuClicked={isHamMenuClicked}
+      />
+      {sideBarVisibility && <Sidebar />}
       <Banner />
       <Intro />
       <Description />
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,19 @@
-function Navbar() {
+function Navbar({ handleSideBar, isHamMenuClicked }) {
   const navItems = ["Home", "Services", "About", "Contact"];
   return (
     <>
       <div className="nav-bar">
+        <button onClick={handleSideBar} className="ham-menu">
+          {isHamMenuClicked ? (
+            <i class="fa-solid fa-xmark"></i>
+          ) : (
+            <i class="fa-solid fa-bars"></i>
+          )}
+        </button>
         <div className="logo">
-          <p style={{ color: "white" }}>Aphelion Labs Inc.</p>
+          <p style={{ color: "white" }}>
+            <i class="fa-solid fa-meteor"></i>Aphelion Labs Inc.{" "}
+          </p>
         </div>
         <ul className="nav-list">
           {navItems.map((item) => (
