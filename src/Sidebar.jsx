beadde648a3afa0a890068e87521ced5fdb6b559
Abhishek <abhishek.kabi.21@gmail.com>
2025-11-11 13:36:35 +0530

    Added new features

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,7 +1,17 @@
 function Sidebar() {
+  const sidebarItems = ["Home", "Products", "Services", "Contact"];
+
   return (
     <>
-      <div className="sidebar"></div>
+      <div className="sidebar">
+        <div className="sidebar-content">
+          {sidebarItems.map((item) => (
+            <a key={item} href="#" className="sidebar-link">
+              {item}
+            </a>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
